1bb88d799d296563847f7dc11fde403907f10d90
Alessandro <alenegri92@gmail.com>
2016-03-12 11:10:18 +0100

    Graphical improvements and Yahoo finance API

--- main_regression.py
+++ main_regression.py
@@ -31,7 +31,6 @@ def connection_to_db(path):
                                  cursorclass=pymysql.cursors.DictCursor)
     return connection
 
-
 def fetch_data_from_db(db, company):
     """
     All data contanining the stock price info are retrieved from the database given the stock name
@@ -60,8 +59,8 @@ def draw_window(my_dpi, data, max_error):
     :param max_error: maximum error allowed
     """
 
-    fig = plt.figure(figsize=(1240/my_dpi, 700/my_dpi), dpi=96, edgecolor='k', facecolor='black')
-    fig.suptitle("PIECEWISE SEGMENTATION REGRESSION", fontsize="15", color="white")
+    fig = plt.figure(figsize=(1000/my_dpi, 700/my_dpi), dpi=96, facecolor='black')
+    fig.suptitle("PIECEWISE SEGMENTATION REGRESSION", fontsize="15", color="white", fontweight='bold', bbox={'facecolor':'red', 'alpha':0.5, 'pad':10})
 
     try:
         stockFile = []
@@ -79,13 +78,12 @@ def draw_window(my_dpi, data, max_error):
     try:
         date, closep_raw = np.loadtxt(stockFile, delimiter=',', unpack=True,
                                                               converters={0: mdates.bytespdate2num('%Y%m%d')})
-        print(closep_raw)
         closep = closep_raw[::-1]
         # First subplot
         ax1 = plt.subplot2grid((3, 3), (0, 0), colspan=3)
         segments = segment.slidingwindowsegment(closep, fit.regression, fit.sumsquared_error, max_error)
         draw_plot(closep,plt,ax1,"Sliding window with regression")
-        draw_segments(segments)
+        draw_segments(segments,'red')
         plt.ylabel('Stock Price')
         plt.title("Sliding window", color='w')
 
@@ -93,7 +91,7 @@ def draw_window(my_dpi, data, max_error):
         ax2 = plt.subplot2grid((3, 3), (1, 0), colspan=3)
         segments = segment.topdownsegment(closep, fit.regression, fit.sumsquared_error, max_error)
         draw_plot(closep, plt, ax2, "Sliding window with regression")
-        draw_segments(segments)
+        draw_segments(segments,'green')
         plt.ylabel('Stock Price')
         plt.title("Top down", color='w')
 
@@ -101,7 +99,7 @@ def draw_window(my_dpi, data, max_error):
         ax3 = plt.subplot2grid((3, 3), (2, 0), colspan=3)
         segments = segment.bottomupsegment(closep, fit.regression, fit.sumsquared_error, max_error)
         draw_plot(closep, plt, ax3, "Sliding window with regression")
-        draw_segments(segments)
+        draw_segments(segments,'blue')
         plt.ylabel('Stock Price')
         plt.title("Bottom up", color='w')
 
@@ -113,7 +111,7 @@ def draw_window(my_dpi, data, max_error):
 
 def draw_plot(data, plt,ax,plot_title):
     ax.plot(range(len(data)), data, alpha=0.8, color='black')
-    ax.grid(True, color='#949494')
+    ax.grid(True, color='#969696')
     ax.yaxis.label.set_color("w")
     ax.xaxis.label.set_color("w")
     ax.tick_params(axis='y', colors='w')
@@ -123,10 +121,10 @@ def draw_plot(data, plt,ax,plot_title):
     plt.title(plot_title)
     plt.xlim((0, len(data)-1))
 
-def draw_segments(segments):
+def draw_segments(segments,color):
     ax = gca()
     for segment in segments:
-        line = Line2D((segment[0],segment[2]),(segment[1],segment[3]),color="red")
+        line = Line2D((segment[0],segment[2]),(segment[1],segment[3]),color=color)
         ax.add_line(line)
 
 def draw_window_API(my_dpi, max_error, stockToFetch):
@@ -138,7 +136,7 @@ def draw_window_API(my_dpi, max_error, stockToFetch):
     """
 
     fig = plt.figure(figsize=(1000/my_dpi, 700/my_dpi), dpi=96, edgecolor='k', facecolor='black')
-    fig.suptitle("PIECEWISE SEGMENTATION REGRESSION", fontsize="15", color="white")
+    fig.suptitle("PIECEWISE SEGMENTATION REGRESSION", fontsize="15", color="white", fontweight='bold', bbox={'facecolor':'red', 'alpha':0.5, 'pad':10})
 
     try:
         print('Currently Pulling',stockToFetch)
@@ -165,7 +163,7 @@ def draw_window_API(my_dpi, max_error, stockToFetch):
         ax1 = plt.subplot2grid((3, 3), (0, 0), colspan=3)
         segments = segment.slidingwindowsegment(closep, fit.regression, fit.sumsquared_error, max_error)
         draw_plot(closep,plt,ax1,"Sliding window with regression")
-        draw_segments(segments)
+        draw_segments(segments,'red')
         plt.ylabel('Stock Price')
         plt.title("Sliding window", color='w')
 
@@ -173,7 +171,7 @@ def draw_window_API(my_dpi, max_error, stockToFetch):
         ax2 = plt.subplot2grid((3, 3), (1, 0), colspan=3)
         segments = segment.topdownsegment(closep, fit.regression, fit.sumsquared_error, max_error)
         draw_plot(closep, plt, ax2, "Sliding window with regression")
-        draw_segments(segments)
+        draw_segments(segments,'green')
         plt.ylabel('Stock Price')
         plt.title("Top down", color='w')
 
@@ -181,7 +179,7 @@ def draw_window_API(my_dpi, max_error, stockToFetch):
         ax3 = plt.subplot2grid((3, 3), (2, 0), colspan=3)
         segments = segment.bottomupsegment(closep, fit.regression, fit.sumsquared_error, max_error)
         draw_plot(closep, plt, ax3, "Sliding window with regression")
-        draw_segments(segments)
+        draw_segments(segments,'blue')
         plt.ylabel('Stock Price')
         plt.title("Bottom up", color='w')
 
@@ -208,8 +206,8 @@ if __name__ == '__main__':
     res = fetch_data_from_db(connection, stock)
 
     # Figure is built
-    draw_window(MY_DPI, res, float(err))
-    # draw_window_API(MY_DPI, float(err), stock)
+    # draw_window(MY_DPI, res, float(err))
+    draw_window_API(MY_DPI, float(err), stock)
 
 
 
